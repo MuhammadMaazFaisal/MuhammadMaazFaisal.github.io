72ea63b3de94776bad795966a18c703e25ca7b38
M.Maaz Faisal <m.maazfaisal0301@gmail.com>
2022-09-10 23:34:56 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <!-- Created By M.Maaz Faisal -->
 <html lang="en">
@@ -7,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Portfolio Website</title>
+    <link rel="icon" href="lofo/Black-logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
@@ -242,7 +242,7 @@
                             <button type="submit" name="submit">Send message</button>
                         </div>
                     </form>
-                    
+
                 </div>
             </div>
         </div>
